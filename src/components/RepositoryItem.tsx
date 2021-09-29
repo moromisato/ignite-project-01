0c4cd599183ec6dfc1a38fdf181a077d11eee1fa
Luis Moromisato <moromisato.luis@gmail.com>
2021-09-28 23:33:58 -0300

    created repostiory item

---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,18 @@
+import { Repository } from "./RepositoryList";
+
+interface RepositoryItemProps {
+  repository: Repository
+}
+
+export function RepositoryItem({repository}: RepositoryItemProps) {
+  return (
+    <li>
+      <strong>{repository.name}</strong>
+      <p>{repository.description}</p>
+
+      <a href={repository.html_url}>
+        Acessar Repositório
+      </a>
+  </li>
+  )
+}
