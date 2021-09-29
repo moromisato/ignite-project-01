2f41507caa8b2c950cb8b2519c498a7e72ad9574
Luis Moromisato <moromisato.luis@gmail.com>
2021-09-28 23:33:50 -0300

    created repostiory list and its styles

---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,32 @@
+import { RepositoryItem } from "./RepositoryItem";
+import '../styles/respositories.scss'
+import { useEffect, useState } from "react";
+
+export interface Repository {
+  id: string,
+  name: string,
+  description: string,
+  html_url: string
+}
+
+export function RepositoryList() {
+  
+  const [repositories, setRepositories] = useState<Repository[]>([])
+  
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, [])
+
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+      <ul>
+        {repositories.map(repository => (
+          <RepositoryItem key={repository.id} repository={repository}/>
+        ))}
+      </ul>
+    </section>
+  )
+}
